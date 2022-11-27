98b9b31af3823f360ebe552c54606edc13dedb49
Chritopher-Gonzalez <chgo7806@colorado.edu>
2022-11-26 21:05:42 -0700

    Finishing Problem 2

---
+++ Probelem2/smallBottom.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 15:21:31 2022
+
+@author: Christopher
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import data
+df = pd.read_excel("../Data/SmallSphere.xlsx")
+print(df)
+#process data
+# x-axis
+x = df['Time [sec]'].to_numpy()
+x = x[700:738]
+time = np.log(x)
+# y-axis
+y = df['Temperature @ r=R/2 [C]'].to_numpy()
+y = y[700:738]
+temp = np.log(y)
+
+#find line of best fit
+a, b = np.polyfit(time, temp, 1)
+print(a,b)
+print(np.exp(a),np.exp(b))
+#add points to plot
+plt.scatter(x, y)
+#add line of best fit to plot
+plt.plot(x, np.exp(a*time + b ))        
+#log scale
+plt.loglog()
